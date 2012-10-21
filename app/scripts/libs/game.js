abd65a5215a0064b1306a2b38b11cb953a28ea6d
Richard Walker <richie@mediasuite.co.nz>
2012-10-21 13:49:29 +0200

    deletes multiple unneeded images, refactors keyboard handling of player into player. Adds rotation left and right and binds them to the left and right keys

--- app/scripts/libs/game.js
+++ app/scripts/libs/game.js
@@ -1,66 +1,44 @@
 define([],function(){
 
-	var startPosition = {
-		x : 188,
-    	y : 130
-	}
-
-	var stepSpeed = 5;
-	var mvSpeed = 1;
-
-	var keyboard, player, canvas;
-
-	function step() {
-  		
-  		clearCanvas();
-
-  		player.setPosition(player.getX(), player.getY());
-  		
-  		if(keyboard.getKey(37) === true){
-  			player.setPosition(player.getX()-mvSpeed, player.getY());
-  		}
-  		if(keyboard.getKey(38) === true){
-			player.setPosition(player.getX(), player.getY()-mvSpeed);
-  		}
-  		if(keyboard.getKey(39) === true){
-  			player.setPosition(player.getX()+mvSpeed, player.getY());
-  		}
-  		if(keyboard.getKey(40) === true){
-  			player.setPosition(player.getX(), player.getY()+mvSpeed);
-  		}
-                if(keyboard.getKey(32) === true) {
-                        mvSpeed = 3;
-                } else {
-                    mvSpeed = 1;
-                }
-            
-  		window.setTimeout(step,stepSpeed);
-  	};
-
-  	function clearCanvas() {
-    	canvas.getContext()
-			.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+    var startPosition = {
+        x : 188,
+        y : 130
     }
 
-    function init(dependencies) {
+    var stepSpeed = 5;
 
-		keyboard = dependencies.keyboard;
-		player = dependencies.player;
-		canvas = dependencies.canvas;
+    var keyboard, player, canvas;
 
-		//var world = createWorld();
-		canvas.init('game');
+    function step() {
+        clearCanvas();
+        player.update(keyboard);
+        window.setTimeout(step,stepSpeed);
+    };
 
-		window.addEventListener('keydown',keyboard.handleKeyDown,true);
-		window.addEventListener('keyup',keyboard.handleKeyUp,true);
+function clearCanvas() {
+    canvas.getContext()
+    .clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+}
 
-		player.init(startPosition, canvas.getContext());
+function init(dependencies) {
 
-		step();
-    }
+    keyboard = dependencies.keyboard;
+    player = dependencies.player;
+    canvas = dependencies.canvas;
 
-    return {
-    	'init' : init
-    }
+    //var world = createWorld();
+    canvas.init('game');
+
+    window.addEventListener('keydown',keyboard.handleKeyDown,true);
+    window.addEventListener('keyup',keyboard.handleKeyUp,true);
+
+    player.init(startPosition, canvas.getContext());
+
+    step();
+}
+
+return {
+    'init' : init
+}
 
 });
--- app/scripts/libs/player.js
+++ app/scripts/libs/player.js
@@ -1,18 +1,38 @@
 define([],function(){
 
-	var player = {
-		'radius' : 10
+    var player = {
+		'radius' : 10,
+		'image' : 'images/spaceship1.jpg',
+		'initialRotation' : 0,
+		'height' : 32,
+		'width' : 32,
+        'moveSpeed' : 1,
+        'swivelIncrements' : 126
+
 	};
 
-	var context, positionX, positionY;
+	var image, context, positionX, positionY, angle, currentRotation;
 
-	function setPosition(x, y) {
-		positionX = x;
+	function setPosition(x, y, rotation) {
+		
+        //store position and rotation
+        positionX = x;
 		positionY = y;
-		context.beginPath();
-		context.arc(x, y, player.radius, 0, Math.PI*2, true); 
-		context.closePath();
-		context.fill();
+
+        //save the context so we can do a transform
+        context.save();
+        //create the transformation
+        //context.translate(100, 100);
+        context.translate(x+(player.width/2), y+(player.width/2));
+        context.rotate(angle * rotation);
+        //setPosition(-16, -16);
+        context.drawImage(image,-(player.width/2),-(player.width/2));
+        
+        //restore the context now that we have performed the
+        //transformations
+        context.restore();
+
+        //context.drawImage(image, x, y, player.height, player.width);
 	}
 
 	function getX() {
@@ -23,13 +43,91 @@ define([],function(){
 		return positionY;
 	}
 
+    function setX(x) {
+        positionX = x;
+    }
+
+    function setY(y) {
+        positionY = y;
+    }
+
+    function update(keyboard) {
+ 
+        if(keyboard.getKey(37) === true){
+            swivelLeft();
+        }
+        else if(keyboard.getKey(38) === true){
+            moveForward();
+        }
+        else if(keyboard.getKey(39) === true){
+            swivelRight();
+        }
+        else if(keyboard.getKey(40) === true){
+            moveBackward();
+        }
+        else {
+            setPosition(getX(), getY(), currentRotation);
+        }
+        
+        if(keyboard.getKey(32) === true) {
+            player.mvSpeed = 3;
+        } else {
+            player.mvSpeed = 1;
+        }
+    }
+
+    function moveLeft() {
+        setPosition(getX()-player.mvSpeed, getY(), currentRotation);
+    }
+
+    function moveRight() {
+        setPosition(getX()+player.mvSpeed, getY(), currentRotation);
+    }
+
+    function moveUp() {
+        setPosition(getX(), getY()-player.mvSpeed, currentRotation);
+    }
+
+    function moveDown() {
+        setPosition(getX(), getY()+player.mvSpeed, currentRotation);
+    }
+
+    function swivelLeft() {
+        currentRotation++;
+        currentRotation = currentRotation % player.swivelIncrements;
+        setPosition(getX(), getY(), currentRotation);
+    }
+
+    function swivelRight() {
+        currentRotation--;
+        currentRotation = currentRotation % player.swivelIncrements;
+        setPosition(getX(), getY(), currentRotation);
+    }
+
+    function moveForward() {
+
+    }
+
+    function moveBackward() {
+
+    }
+
 	function init(startPosition, ctx) {
 		context = ctx;
-		setPosition(startPosition.x, startPosition.y);
+        currentRotation = 0;
+        //360 degrees broken into 32 bits in radians
+        angle = 2 * Math.PI / player.swivelIncrements;
+		image = new Image();
+		image.src = player.image;
+		image.width = player.width;
+		image.height = player.height;
+		image.onload = function(){
+			setPosition(startPosition.x, startPosition.y);
+		};
 	}
 
 	return {
-		'setPosition' : setPosition,
+		'update' : update,
 		'getX' : getX,
 		'getY' : getY,
 		'init' : init
